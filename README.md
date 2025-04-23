7e0617525b153989b28abd4951d887089e8ce099
leedongyeop <ldy_1204@naver.com>
2025-04-23 22:58:01 +0900

    docs : 4-3. Kotlin에서 람다를 다루는 방법

--- README.md
+++ README.md
@@ -264,7 +264,44 @@
 
 <br/>
 
-### 4-3. Kotlin에서 람다를 다루는 방법
+### 4-3. [Kotlin에서 람다를 다루는 방법](src/main/java/io/dongvelop/lec17/Lec17Main.kt)
+
+- 함수는 Java에서는 2급 시민이지만, **코틀린에서는 1급 시민**이다.
+    - 때문에 함수 자체를 변수에 넣을 수도 있고, 파라미터로 전달할 수도 있다.
+- 코틀린에서 람다는 두 가지 방법으로 만들 수 있고, `{ }` 방법이 더 많이 사용된다.
+  ```kotlin
+  // 람다를 직접 호출하는 방법 1
+  val isApple  = fun(fruit: Fruit): Boolean {
+      return fruit.name == "사과"
+  }
+  
+  // 람다를 직접 호출하는 방법 2
+  val isApple2 = { fruit: Fruit -> fruit.name == "사과" }
+  ```
+- 함수를 호출하며, 마지막 파라미터인 람다를 쓸 때는 소괄호 밖으로 람다를 뺄 수 있다.
+  ```kotlin
+  // AS-IS
+  val filteredFruit2 = filterFruit(fruits, { fruit: Fruit -> fruit.name == "사과" })
+  
+  // TO-BE
+  val filteredFruit3 = filterFruit(fruits) { fruit: Fruit -> fruit.name == "사과" }
+  ```
+- 파라미터가 한 개인 람다를 쓸 때에는, `it`을 통해 접근할 수 있다.
+  ```kotlin
+  // 호출 예시 Step 4 : fruit 변수의 타입이 추론이 가능하므로 생략.
+  val filteredFruit4 = filterFruit(fruits) { fruit -> fruit.name == "사과" }
+  
+  // 호출 예시 Step 5 : fruit 변수 간소화
+  val filteredFruit5 = filterFruit(fruits) { it.name == "사과" }
+  ```
+- 람다의 마지막 expression 결과는 람다의 반환 값이다.
+  ```kotlin
+  filterFruit(fruits) {
+      println("사과만 받는다.")
+      it.name == "사과" // return value
+  }
+  ```
+- 코틀린에서는 `Closure`를 사용하여 `non-final` 변수도 람다에서 사용할 수 있다.
 
 <br/>
 
---
+++ src/main/java/io/dongvelop/lec17/Lec17Main.kt
@@ -0,0 +1,65 @@
+package io.dongvelop.lec17
+
+/**
+ * @author 이동엽(Lee Dongyeop)
+ * @date 2025. 04. 23
+ * @description Kotlin에서 람다를 다루는 방법
+ */
+fun main() {
+    val fruits = listOf(
+        Fruit("사과", 1_000),
+        Fruit("사과", 1_200),
+        Fruit("사과", 1_200),
+        Fruit("사과", 1_500),
+        Fruit("바나나", 3_000),
+        Fruit("바나나", 3_200),
+        Fruit("바나나", 2_500),
+        Fruit("수박", 10_000),
+    )
+
+    // 람다를 직접 호출하는 방법 1
+    // 함수의 이름이 없으며, 함수의 타입을 명시 가능 (Fruit을 받아, Boolean으로 리턴)
+    val isApple: (Fruit) -> Boolean = fun(fruit: Fruit): Boolean {
+        return fruit.name == "사과"
+    }
+
+    // 호출 방식 (직접 , invoke() 이용)
+    isApple(fruits[0])
+    isApple.invoke(fruits[0])
+
+    // 람다를 직접 호출하는 방법 2
+    val isApple2 = { fruit: Fruit -> fruit.name == "사과" }
+
+    // 호출 예시 Step1
+    val filteredFruit = filterFruit(fruits, isApple)
+
+    // 호출 예시 Step2 : 함수 자체를 선언
+    val filteredFruit2 = filterFruit(fruits, { fruit: Fruit -> fruit.name == "사과" })
+
+    // 호출 예시 Step 3 : 소괄호 내에 중괄호가 있는 것이 어색하니, 바깥으로 분리
+    val filteredFruit3 = filterFruit(fruits) { fruit: Fruit -> fruit.name == "사과" }
+
+    // 호출 예시 Step 4 : fruit 변수의 타입이 추론이 가능하므로 생략.
+    val filteredFruit4 = filterFruit(fruits) { fruit -> fruit.name == "사과" }
+
+    // 호출 예시 Step 5 : fruit 변수 간소화
+    val filteredFruit5 = filterFruit(fruits) { it.name == "사과" }
+}
+
+// Kotlin에서는 매개변수로 함수 자체를 넘길 수 있음.
+fun filterFruit(
+    fruits: List<Fruit>,
+    filter: (Fruit) -> Boolean
+): List<Fruit> {
+
+    val results = mutableListOf<Fruit>()
+
+    for (fruit in fruits) {
+        if (filter(fruit)) {
+            results.add(fruit)
+        }
+    }
+
+    return results
+//    return fruits.filter(filter)
+}
